c9741ec0a443d3ba787e72d8e6ca8d16397381da
Aditya <adidtyaad@gmail.com>
2024-03-04 14:56:20 +0700

    🧱 build: update prototype ui detail page

--- src/pages/detail_page/DetailPage.jsx
+++ src/pages/detail_page/DetailPage.jsx
@@ -4,27 +4,32 @@ function DetailPage() {
   return (
     <section>
       <div className="min-h-screen bg-opacity-90 text-white">
-        <div className="h-screen flex w-full bg-slate-500 space-x-8 items-center">
-          <div className="flex w-full space-x-12 flex-row h-80 justify-center items-center">
+        <div
+          className="h-screen flex w-full bg-cover bg-center bg-no-repeat relative"
+          style={{
+            backgroundImage: `url('https://m.media-amazon.com/images/M/MV5BMTEyOTQzZjgtMDM1OC00MWMxLWI2ZGUtYWUwOTQxNTRmZTU0XkEyXkFqcGdeQXVyNTU1MDIzMzg@._V1_FMjpg_UX1000_.jpg')`,
+          }}>
+          <div className="absolute inset-x-0 bottom-0 h-96 bg-gradient-to-t from-black opacity-100" />
+          <div className="flex h-full w-full space-x-12 flex-row justify-center items-center">
             <img
               src={`https://m.media-amazon.com/images/M/MV5BMTEyOTQzZjgtMDM1OC00MWMxLWI2ZGUtYWUwOTQxNTRmZTU0XkEyXkFqcGdeQXVyNTU1MDIzMzg@._V1_FMjpg_UX1000_.jpg`}
               alt="No Way Up"
-              className=" w-64 rounded-md"
+              className="w-64 rounded-md"
             />
-            <div className="space-y-4 mt-auto">
-              <h1 className="text-3xl font-semibold">No Way Up</h1>
+            <div className="space-y-4  text-white">
+              <h1 className="text-3xl font-semibold mt-14">No Way Up</h1>
               <div className="flex-row space-x-2">
-                <p className="border rounded-full inline-block px-4 text-center ">
+                <p className="border rounded-full inline-block px-4 text-center">
                   Action
                 </p>
-                <p className="border rounded-full inline-block px-4 text-center ">
+                <p className="border rounded-full inline-block px-4 text-center">
                   Comedy
                 </p>
-                <p className="border rounded-full inline-block px-4 text-center ">
+                <p className="border rounded-full inline-block px-4 text-center">
                   Slice of Life
                 </p>
               </div>
-              <h1 className=" max-w-xl">
+              <h1 className="max-w-xl">
                 Characters from different backgrounds are thrown together when
                 the plane they're travelling on crashes into the Pacific Ocean.
                 A nightmare fight for survival ensues with the air supply
@@ -33,14 +38,14 @@ function DetailPage() {
               <h1 className="text-xl font-bold">Top Casts</h1>
               <div className="flex space-x-4">
                 <div className="flex flex-col">
-                  <div className=" h-20 w-16 max-w-40 rounded-md bg-slate-400" />
-                  <p className="mx-auto mt-2 text-opacity-50 text-white line-clamp-2 text-center w-16">
+                  <div className="h-20 w-16 max-w-40 rounded-md bg-slate-400" />
+                  <p className="mx-auto mt-2 text-opacity-50 line-clamp-2 w-16">
                     Aditya Ade
                   </p>
                 </div>
                 <div className="flex flex-col">
-                  <div className=" h-20 w-16 max-w-40 rounded-md bg-slate-400" />
-                  <p className="mx-auto mt-2 text-opacity-50 text-white line-clamp-2 text-center w-16">
+                  <div className="h-20 w-16 max-w-40 rounded-md bg-slate-400" />
+                  <p className="mx-auto mt-2 text-opacity-50 line-clamp-2 w-16">
                     Maylida Dwi Chairunnisa
                   </p>
                 </div>
